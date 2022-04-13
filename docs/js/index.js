d218a99ab32034716e1c700a027a6f9ca62c17ad
Torcoroma <torcoromaarias@gmail.com>
2022-04-13 16:39:15 -0500

    Arreglos portafolio

--- docs/js/index.js
+++ docs/js/index.js
@@ -2,19 +2,19 @@
   AOS.init();
 })();
 const header = document.getElementById("header");
-const inicio = document.getElementById("inicio");
-const acerca = document.getElementById("acerca");
-const conocer = document.getElementById("conocer");
-const proyecto = document.getElementById("proyectos");
-const contacto = document.getElementById("contactos");
-console.log(inicio, acerca, conocer, proyecto, contacto);
+const inicio = document.getElementById("home");
+const acerca = document.getElementById("onmi");
+const conocer = document.getElementById("servicess");
+const proyecto = document.getElementById("proyect");
+const contacto = document.getElementById("contacts");
+console.log(inicio, acerca, conocer, proyecto, contacto, header);
 const CambioColor = (event) => {
   inicio.classList.remove("active");
   acerca.classList.remove("active");
   conocer.classList.remove("active");
   acerca.classList.remove("active");
   proyecto.classList.remove("active");
-  contactos.classList.remove("active");
+  contacto.classList.remove("active");
 
   const link = event.target;
   console.log(link);
