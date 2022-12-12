140c5eb8b676743381c7855820ffeb016a083533
Torcoroma <torcoromaarias@gmail.com>
2022-12-12 18:04:20 -0500

    boton js nueva verciobn

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,26 +1,24 @@
 const li = document.querySelectorAll(".nav-item");
-const bloque = document.querySelectorAll(".taps");
-const li2 = document.querySelectorAll(".nav-item");
-const bloque2 = document.querySelectorAll(".taps2");
-
 li.forEach((cadaLi, i) => {
   li[i].addEventListener("click", () => {
     li.forEach((cadaLi, i) => {
       li[i].classList.remove("active");
-      bloque[i].classList.remove("active");
     });
     li[i].classList.add("active");
-    bloque[i].classList.add("active");
   });
 });
 
-// li2.forEach((cadali2, i) => {
-//   li2[i].addEventListener("click", () => {
-//     li2.forEach((cadali2, i) => {
-//       li2[i].classList.remove("actives");
-//       bloque2[i].classList.remove("actives");
-//     });
-//     li2[i].classList.add("actives");
-//     bloque2[i].classList.add("actives");
-//   });
-// });
+// este es el escrol
+
+const boton = document.getElementById("boton");
+
+window.onscroll = () => {
+  let scrollPunto = window.pageYOffset;
+  if (scrollPunto <= 600) {
+    boton.style.display = "none";
+    boton.style.transition = "0.8s";
+  } else {
+    boton.style.display = "block";
+    boton.style.transition = "1s";
+  }
+};
